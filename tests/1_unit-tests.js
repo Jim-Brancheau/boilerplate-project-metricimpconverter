835ba76f859994b5fc0a2e5bbb4250b88b1a2fa0
Jim-Brancheau <Jim.brancheau@gmail.com>
2023-06-28 02:08:47 +0000

    test skeleton

---
+++ tests/1_unit-tests.js
@@ -0,0 +1,57 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+suite("convertHandler", function() {
+  suite("Input Validation", function() {
+    test("convertHandler should correctly read a whole number input.", function(){
+      
+    });
+    test("convertHandler should correctly read a decimal number input.", function(){
+      
+    });
+    test("convertHandler should correctly read a fractional input.", function(){
+      
+    });
+    test("convertHandler should correctly read a fractional input with a decimal.", function(){
+      
+    });
+    test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).", function(){
+      
+    });
+    test("convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.", function(){
+      
+    });
+    test("convertHandler should correctly read each valid input unit.", function(){
+      
+    });
+    test("convertHandler should correctly return an error for an invalid input unit.", function(){
+      
+    });
+    test("convertHandler should return the correct return unit for each valid input unit.", function(){
+      
+    });
+    test("convertHandler should correctly return the spelled-out string unit for each valid input unit.", function(){
+      
+    });
+  })
+  suite("Conversion", function() {
+    test("convertHandler should correctly convert gal to L.", function(){
+      
+    });
+    test("convertHandler should correctly convert L to gal.", function(){
+      
+    });
+    test("convertHandler should correctly convert mi to km.", function(){
+      
+    });
+    test("convertHandler should correctly convert km to mi.", function(){
+      
+    });
+    test("convertHandler should correctly convert lbs to kg.", function(){
+      
+    });
+    test("convertHandler should correctly convert kg to lbs.", function(){
+      
+    });
+  } )
+})
