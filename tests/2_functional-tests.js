a89761512503932d9b9cd1c472b0d40b57c7babb
Jim Brancheau <jim.brancheau@gmail.com>
2023-07-19 23:16:49 -0400

    remove commented code

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -74,36 +74,3 @@ suite("Functional Tests", function () {
     });
   });
 });
-
-// const Browser = require("zombie");
-// Browser.site = "https://boilerplate-mochachai--jamesbrancheau.repl.co";
-
-// suite("Functional Tests with Zombie.js", function () {
-//   this.timeout(10000);
-//   const browser = new Browser();
-
-//   suiteSetup(function (done) {
-//     return browser.visit("/", done);
-//   });
-
-//   suite("Headless browser", function () {
-//     test('should have a working "site" property', function () {
-//       assert.isNotNull(browser.site);
-//     });
-//   });
-
-//   suite('"Famous Italian Explorers" form', function () {
-//     // #5
-//     test('Submit the surname "Colombo" in the HTML form', function (done) {
-//       browser.fill("surname", "Colombo").then(() => {
-//         browser.pressButton("submit", () => {
-//           browser.assert.success();
-//           browser.assert.text("span#name", "Cristoforo");
-//           browser.assert.text("span#surname", "Colombo");
-//           browser.assert.elements("span#dates", 1);
-//           done();
-//         });
-//       });
-//     });
-//   });
-// });
